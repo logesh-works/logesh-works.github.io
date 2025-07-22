7dd3ac8ba624b747d23df5454b57a158bd56c0e9
Logesh Kumar G <logeshkumar.dev@gmail.com>
2025-07-23 01:46:37 +0530

    added new resume

--- components/work/IndustryExperience.tsx
+++ components/work/IndustryExperience.tsx
@@ -11,22 +11,22 @@ const IndustryExperience = () => {
       <Experience
           company="Cyces Innovation Labs LLP"
           position="Software Development Engineer"
-          startDate="Jan - 2025"
+          startDate="Jan 2025"
           endDate="Present"
           description="I have been working as a Software Development Engineer, where I design, develop, and optimize software solutions tailored to meet business needs. My role involves working on backend and frontend development, ensuring high-performance and scalable applications. I collaborate with cross-functional teams to implement new features, troubleshoot issues, and enhance system efficiency."
         />
         <Experience
           company="FreeLancer"
           position="Full-Stack Web Developer"
-          startDate="May - 2022"
-          endDate="Jan - 2025"
+          startDate="May 2022"
+          endDate="Jan 2025"
           description="I've always been enjoying to experiment new technologies, working with people coming from different backgrounds and creating products that solve companies & people needs.You can find all of my latest projects on GitHub by visiting my profile at GitHub. Check them out to explore the diverse range of technologies and tools I've used. I'm always open to feedback and collaboration, so feel free to contact me , Happy coding! ."
         />
         <Experience
           company="Tutor at CSC Annamalainagar branch"
           position="Tech Instructor."
-          startDate="Jan - 2022"
-          endDate="Jan - 2025"
+          startDate="Jan 2022"
+          endDate="Jan 2025"
           description="Conducted interactive coding classes for students of various skill levels.
 Developed lesson plans and instructional materials to effectively teach coding concepts.
 Provided guidance and support to students in their coding projects and assignments.
